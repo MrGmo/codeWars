199aa235f9eadb75a697379e516225b33f91a281
Guillermo Aguilar <gaguilar128@gmail.com>
2021-04-26 02:09:42 -0500

    Practice

---
+++ index.js
@@ -0,0 +1,10 @@
+// 8 kyu - Coefficients of the Quadratic Equation
+// const quadratic = (a, b) => [1,(a+b)*-1, a*b]
+
+// 8 kyu - Smallest unused ID
+// function nextId(ids){
+//   const used = new Set(ids);
+//   for (let i = 0; i <= ids.length; i++) {
+//     if (!used.has(i)) return i;
+//   }
+// }
