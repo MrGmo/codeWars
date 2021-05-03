93d2c06c12bdf4205edd64a1a7f0af4adcb7c576
Guillermo Aguilar <gaguilar128@gmail.com>
2021-05-03 12:02:11 -0500

    Algo Practice

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+// 8kyu - Training JS #4: Basic Data Types - Array
+// function getLength(arr, num){
+//   return arr.length
+// }
+// function getFirst(arr){
+//   return arr[0]
+// }
+// function getLast(arr){
+//   return arr.pop()
+// }
+// function pushElement(arr, num){
+//   arr = [...arr]
+//   arr.push(num)
+//   return arr
+// }
+// function popElement(arr){
+//   return arr.slice(0,-1)
+// }
+
+
 // 7kyu - Basic Math (Add or Subtract)
 
 // function calculate(str){
