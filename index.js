e61affa223f203ebc5dc0bf886993bf8537df6e9
Guillermo Aguilar <gaguilar128@gmail.com>
2021-05-02 19:06:37 -0500

    Algo Practice

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+// 7kyu - Basic Math (Add or Subtract)
+
+// function calculate(str){
+//   const newStr = str.replaceAll('plus', '+').replaceAll('minus', '-')
+//   return eval(newStr).toString()
+// }
+
+// console.log(calculate('1plus2plus3plus4'))
+
+
 // 8kyu - Points of Reflection
 
 // function symmetricPoint(p,q) { 
