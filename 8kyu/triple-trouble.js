74c094d0cc806d270e694cfa08cf40f526ccca54
Guillermo Aguilar <gaguilar128@gmail.com>
2021-06-07 19:46:02 -0500

    Codewars practice

---
+++ 8kyu/triple-trouble.js
@@ -0,0 +1,9 @@
+function tripleTrouble(one, two, three){
+  let arr = []
+  for(let i = 0; i <= one.length; i++){
+    arr.push(one[i])
+    arr.push(two[i])
+    arr.push(three[i])
+  }
+  return arr.join('')
+ }
