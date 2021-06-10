7bf386a7c2e76947716af8de2870d8f7c47046a1
Guillermo Aguilar <gaguilar128@gmail.com>
2021-06-09 21:16:13 -0500

    Codewars practice

---
+++ 6kyu/find-the-odd-int.js
@@ -0,0 +1,12 @@
+function findOdd(numbers) {
+  let count = 0;
+  for(let i = 0; i < numbers.length; i++){
+    for(let j = 0; j < numbers.length; j++){
+      if(numbers[i] == numbers[j]){
+        count++;
+      }
+    }
+    if(count % 2 != 0 ){
+      return numbers[i];
+    }
+  }
